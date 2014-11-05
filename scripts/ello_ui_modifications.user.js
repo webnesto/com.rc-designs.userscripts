56cdafe73e583b27261393b747c81c91bc0b09df
Ernesto <nesto@rc-designs.com>
2014-11-05 11:29:32 -0800

    Update ello_ui_modifications.user.js

--- scripts/ello_ui_modifications.user.js
+++ scripts/ello_ui_modifications.user.js
@@ -2,7 +2,7 @@
 // @name           Ello - UI Modifications
 // @description    Ello - Various UI mods to improve UX for me
 // @include        https://ello.co/*
-// @version        1.1
+// @version        1.2
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
@@ -22,7 +22,7 @@ var styles = {
 	".comments .comment" : [
 		"padding: 1em;"
 	]
-,	".comments .comment ..comment__header" : [
+,	".comments .comment .comment__header" : [
 		"top: 1em;"
 	,	"left: 1em;"
 	]

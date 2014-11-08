d68c573c6441307d10cc9b40498d280898f7b996
ernesto johnson <nesto@rc-designs.com>
2014-11-08 15:47:46 -0800

    Added Facebook script... cause I'm really sick of that damn blue

---
+++ scripts/facebook_ui_improvements.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name           Facebook - UI Improvments
+// @description    Facebook - UI Improvments
+// @include        https://www.facebook.com/*
+// @version        1.0
+// ==/UserScript==
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+// function addJQuery(callback) {
+//   var script = document.createElement("script");
+//   script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+//   script.addEventListener('load', function() {
+//     var script = document.createElement("script");
+//     script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+//     document.body.appendChild(script);
+//   }, false);
+//   document.body.appendChild(script);
+// }
+
+var styles = {
+	'#blueBarNAXAnchor' : [
+		"background: #333333;"
+	]
+,	'a.jewelButton[name="requests"]' : [
+		"background-position: 0 -511px;"
+	]
+,	'a.jewelButton[name="mercurymessages"]' : [
+		"background-position: 0 -320px;"
+	]
+,	'a.jewelButton[name="notifications"]' : [
+		"background-position: 0 -607px;"
+	]
+,	'#privacyFlyoutLabel' : [
+		"background-position: 0 -370px;"
+	]
+,	'#userNavigationLabel' : [
+		"background-position: -252px -100px;"
+	]
+};
+
+for( var rule in styles ){
+	GM_addStyle( rule + "{" + styles[ rule ].join( "" ) + "}" );
+}
+
+
+// addJQuery( function(){
+	
+
+// });

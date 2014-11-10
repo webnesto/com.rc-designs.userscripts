fc414632b44be66ddaf33fea9c597072c85ba4b7
ernesto johnson <nesto@rc-designs.com>
2014-11-10 11:16:16 -0800

    new attempt at correcting flyout rendering

--- scripts/facebook_ui_improvements.user.js
+++ scripts/facebook_ui_improvements.user.js
@@ -2,7 +2,7 @@
 // @name           Facebook - UI Improvments
 // @description    Facebook - UI Improvments
 // @include        https://www.facebook.com/*
-// @version        1.5
+// @version        1.6
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
@@ -38,11 +38,14 @@ var styles = {
 		"background-position: -252px -100px;"
 	]
 	//links
-,	'a, a span, #fbNotificationsFlyout a, #fbNotificationsFlyout a span': [
+,	'a, a span': [
 		"color: #350B1D;"
 	]
 ,	'#blueBarNAXAnchor a, #blueBarNAXAnchor a span': [
 		"color: #ffffff;"
+	]
+,	'#fbNotificationsFlyout a, #fbNotificationsFlyout a span': [
+		"color: #350B1D;"
 	]
 };
 

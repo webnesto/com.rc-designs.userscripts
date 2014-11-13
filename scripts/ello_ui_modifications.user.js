ff9d1e32745b04918a76225bdd6516751a292b1f
B. Ernesto Johnson <nesto@rc-designs.com>
2014-11-13 10:29:17 -0800

    Updated comment styles

--- scripts/ello_ui_modifications.user.js
+++ scripts/ello_ui_modifications.user.js
@@ -2,20 +2,20 @@
 // @name           Ello - UI Modifications
 // @description    Ello - Various UI mods to improve UX for me
 // @include        https://ello.co/*
-// @version        1.10
+// @version        1.11
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
-function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-}
+// function addJQuery(callback) {
+//   var script = document.createElement("script");
+//   script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+//   script.addEventListener('load', function() {
+//     var script = document.createElement("script");
+//     script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+//     document.body.appendChild(script);
+//   }, false);
+//   document.body.appendChild(script);
+// }
 
 var styles = {
 	//improve comment display
@@ -50,13 +50,24 @@ var styles = {
 		"left: 0;"
 	,	"padding-left: 2.8125rem;"
 	]
+,	".comment-editor--focused .editor__actions" : [
+		"height: 2rem;"
+	,	"bottom: -2rem;"
+	]
+,	".js-comment-btn.js-submit" : [
+		"height: 2rem;"
+	,	"line-height: 1rem;"
+	]
+,	".btn--ico .svgicon" : [
+		"height: 2rem;"
+	]
 };
 
 for( var rule in styles ){
 	GM_addStyle( rule + "{" + styles[ rule ].join( "" ) + "}" );
 }
 
-addJQuery( function(){
+// addJQuery( function(){
 	
 
-});
+// });

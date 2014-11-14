55a9915fe79a0c1cf4a41e6f95a138753c9966fa
B. Ernesto Johnson <nesto@rc-designs.com>
2014-11-14 12:30:49 -0800

    * rev'd for updates

--- scripts/ello_ui_modifications.user.js
+++ scripts/ello_ui_modifications.user.js
@@ -2,7 +2,7 @@
 // @name           Ello - UI Modifications
 // @description    Ello - Various UI mods to improve UX for me
 // @include        https://ello.co/*
-// @version        1.12
+// @version        1.13
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading

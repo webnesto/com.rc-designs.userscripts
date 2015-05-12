1013558fe76ab416c9bea36f42b398a393aaf78f
B. Ernesto Johnson <nesto@rc-designs.com>
2015-05-12 09:24:39 -0700

    * version increment

--- scripts/mightytext_ui_improvements.user.js
+++ scripts/mightytext_ui_improvements.user.js
@@ -2,7 +2,7 @@
 // @name           MightyText - UI Improvments
 // @description    MightyText - UI Improvments
 // @include        https://mightytext.net/*
-// @version        1.14
+// @version        1.15
 // ==/UserScript==
 
 a function that loads jQuery and calls a callback function when jQuery has finished loading
